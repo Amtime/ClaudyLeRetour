e9b2171a90c9f35be8f7be945873e551e4b03f9c
Amtime <kilian.favero@hotmail.fr>
2016-11-02 01:16:43 +0100

    Ajout de fonction de developpement DES

--- GS15lib.py
+++ GS15lib.py
@@ -15,14 +15,25 @@ def identite_bezout(a, b, i=0, A=0, B=0, x0=1, y0=0, x1=0, y1=1):
             print("{} = {}*{} + {} <==> {} = {}*{} - {}*{}".format(a, b, q, r, r, A, x, B, y))
             print("x{} = q1*x1 + x0 <==> {} = {}*{} + {}".format(i+2, x, q, x1, x0))
             print("y{} = q1*y1 + y0 <==> {} = {}*{} + {}\n".format(i+2, y, q, y1, y0))
-        return(identite_bezout+(b, r, i = i+1, A=A, B=B, x1=x, y1=y, x0=x1, y0=y1))
+        return(identite_bezout(b, r, i=i+1, A=A, B=B, x1=x, y1=y, x0=x1, y0=y1))
+
+def fonction_de_developpement_DES(listeInput):
+    """Attends une liste d'entier en caractère et la développe
+        32 --> 48
+    """
+    listeOutput = listeInput[0:5]; listeOutput.insert(0, listeInput[-1])
+    for i in range(1,7):
+        listeOutput += listeInput[i*4-1:i*4+5]
+    listeOutput += listeInput[-5:]; listeOutput.append(listeInput[0])
+    return(listeOutput)
+
 
 def nb_premiers():
     pass
 
 def main():
-    if DEBUG:
-        am1 = identite_bezout(13, 7)
+    pass
+
 
 if __name__ == "__main__":
     main()
