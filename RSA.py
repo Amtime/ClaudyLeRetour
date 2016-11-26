200fb1e6af6a947ce8b36fd572a076cf71045180
Alex <alx.bec@me.com>
2016-11-26 11:30:03 +0100

    RSA 2
    Generation de clé

--- RSA.py
+++ RSA.py
@@ -1,12 +1,33 @@
+from GS15lib import *
+import random
+
 #   Chiffrement RSA
 #   Detail des etapes
 
 # 1. Generation des cles
 #
 #       Choix de N et P deux nombres premiers distincts
+N = 1151
+P = 76733
+
 #       Calculer leur produit NP = Module de chiffrement
+NP = N*P
+print(NP)
+
 #       Calculer l'indicatrice d'Euler PhiN (Ici N-1 x P-1)
-#       e : Choisir un entier naturel e premier avec PhiN
+PHI = (N-1)*(P-1)
+print(PHI)
+
+#       E : Choisir un entier naturel e premier avec PhiN
+#           TQ PGCD(E, P-1) = 1 & PGCD(E, Q-1) = 1
+#           TQ PGCD(E, PHI) = 1
+
+tuple = (0, 0, 0)
+while tuple[0] != 1:
+    E = random.randrange(N, P)
+    tuple = identite_bezout(E, PHI)
+print(E)
+
 #       d : Inverse de e modulo PhiN (inferieur) - Exposant de chiffrement
 #
 #
