419ee0053e79365b9ff6a0c29497a70f6ce540ee
Alex <alx.bec@me.com>
2016-12-07 16:33:45 +0100

    Ecriture clé fichiers
    youpi

--- RSA.py
+++ RSA.py
@@ -4,7 +4,7 @@ import random
 #   Chiffrement RSA
 
 """ 1. Generation des cles
-        TODO Génération nombres premiers
+        TODO Nb premiers : Tableau ou calcul
         OK Clé chiffrement/ déchiffrement
     2. Ecriture des clés vers fichier
         TODO Export fichier
@@ -17,7 +17,7 @@ import random
     7. RSA avec padding PKCS
 """
 
-N = 17
+N = 457
 P = 41
 NP = N*P
 PHI = (N-1)*(P-1)
@@ -28,17 +28,14 @@ liste_dechif = []
 E = random.randint(1,PHI)
 while(COND!=1):
     if(E&1):
-        print("OK E impair")
         PGCDEPHI = identite_bezout(E, PHI)
         COND = 1
         if PGCDEPHI[0]==1:
-            print("Le compte est bon E: ",E)
             COND = 1
         else:
             COND = 0
             E = random.randint(1, PHI)
     else:
-        print("XXXXXXXXX")
         COND = 0
         E = random.randint(1, PHI)
 
@@ -49,6 +46,12 @@ D = identite_bezout(E,PHI)[1]
 print("\nClé privée : ", D)
 
 # 2. Ecriture des clés publiques et privées dans un fichier
+# Définir fonctions pour centraliser R/W fichiers ?
+with open("public_key.txt", "w") as fichier:
+    fichier.write(str(N) + "\n" + str(E))
+
+with open("private_key.txt", "w") as fichier:
+    fichier.write(str(D))
 
 # alternative : open('message.txt', 'r') as message
 message = input('\nEntrez le mot à chiffrer : ')
@@ -63,8 +66,6 @@ for carac in message:
     print("Caractère chiffré : ",carac_crypt, "\n")
     liste_chif.append(carac_crypt)
 print(liste_chif)
-
-
 
 # 4. Dechiffrement avec la clé privée
 
