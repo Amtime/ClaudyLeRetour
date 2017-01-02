34ca85eccbab66d7210e15a11bf6297544f3fcb6
Amtime <kilian.favero@hotmail.fr>
2017-01-02 15:59:53 +0100

    Problème merging

--- GS15lib.py
+++ GS15lib.py
@@ -1,5 +1,7 @@
 from operator import mul, mod
 from functools import reduce
+import random
+
 
 def identite_bezout(a, b, i=0, x0=1, y0=0, x1=0, y1=1):
     """Input  : int, int
@@ -12,6 +14,7 @@ def identite_bezout(a, b, i=0, x0=1, y0=0, x1=0, y1=1):
         return(b, x1, -y1)
     else:
         return(identite_bezout(b, r, i=i+1, x1=x, y1=y, x0=x1, y0=y1))
+
 
 def decoupage_string(string, n):
     """ Decoupe une chaîne de caractère en morceau de n caractères. /!\ Si ça ne tombe pas rond la fonction ignore le reste
@@ -32,8 +35,16 @@ def left_padding(string, char, length):
         output = char + output
     return(output)
 
+
 def right_padding(string, char, length):
     return(string + (length - len(string)) * char)
+
+
+def random_bytes(length, output = ""):
+    while len(output) < length:
+        output = str(random.randint(0, 1)) + output
+    return output
+
 
 
 def main():
